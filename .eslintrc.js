f460c4aa9e33f3e0dcf1f5fbf7fdca3c36c1a567
Aviad Rozenhek <aviadr1@gmail.com>
2025-04-07 21:16:33 +0000

    update lint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.json'],
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'prettier',
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+    env: {
+      node: true,
+      jest: true,
+    },
+  };
